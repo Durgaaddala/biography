f79b9dbcfb1515572257e569c4fcb531f33ecafc
Durgaaddala <135939453+Durgaaddala@users.noreply.github.com>
2023-07-28 23:08:42 -0700

    Add files via upload

---
+++ design.js
@@ -0,0 +1,35 @@
+ReactDOM.render(
+    
+    <div>
+       <h1>Biography of Jawaharlal Nehru </h1>
+       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Jnehru.jpg/220px-Jnehru.jpg" height="300" width="500"/>
+      <p>
+       
+          <b><i>Full name</i></b>	:Jawaharlal Nehru<br/><br/> 
+          <b><i>Born</i></b>	    :14 November 1889<br/><br/> 
+          <b><i>place </i></b>      :Prayagraj<br/><br/> 
+          <b><i>Height	</i></b>    :1.78 m<br/><br/>
+       </p>
+       <h3><u>Jawaharlal Nehru</u></h3>
+       <p>
+       Jawaharlal Nehru, byname Pandit (Hindi: “Pundit” or “Teacher”) Nehru, (born November 14, 1889, Allahabad, India—died May 27, 1964, New Delhi), first prime minister of independent India (1947–64), who established parliamentary government and became noted for his neutralist (nonaligned) policies in foreign affairs. He was also one of the principal leaders of India’s independence movement in the 1930s and ’40s..<br/><br/>
+       </p>
+       <h3><u>Early years</u></h3>
+       <p>
+       Nehru was born to a family of Kashmiri Brahmans, noted for their administrative aptitude and scholarship, who had migrated to Delhi early in the 18th century. He was a son of Motilal Nehru, a renowned lawyer and leader of the Indian independence movement, who became one of Mohandas (Mahatma) Gandhi’s prominent associates. Jawaharlal was the eldest of four children, two of whom were girls. A sister, Vijaya Lakshmi Pandit, later became the first woman president of the United Nations General Assembly.<br/><br/>
+       </p>
+       <h3><u>Nehru was educated</u></h3>
+       <p>
+       Until the age of 16, Nehru was educated at home by a series of English governesses and tutors. Only one of those—a part-Irish, part-Belgian theosophist, Ferdinand Brooks—appears to have made any impression on him. Jawaharlal also had a venerable Indian tutor who taught him Hindi and Sanskrit. [79]<br/><br/>
+       </p>
+       <h3><u>Achievements</u></h3>
+       <p>
+       At the outbreak of World War II in September 1939, the viceroy, Lord Linlithgow, had committed India to war without consulting the autonomous provincial ministries. The Congress Party’s high command withdrew its provincial ministries as a protest, but Congress’s action left the political field virtually open to Jinnah and the Muslim League. Nehru’s views on the war differed from those of Gandhi. Initially, Gandhi believed that whatever support was given to the British should be given unconditionally and that it should be of a nonviolent character. Nehru held that nonviolence had no place in defense against aggression and that India should support Great Britain in a war against Nazism but only as a free country. If it could not help, it should not hinder.<br/><br/>
+       </p>
+       <h3><u>Achievements as prime minister</u></h3>
+       <p>
+       In the 35 years from 1929, when Gandhi chose Nehru as president of the Congress session at Lahore, until his death, as prime minister, in 1964, Nehru remained—despite the debacle of the brief conflict with China in 1962—the idol of his people. His secular approach to politics contrasted with Gandhi’s religious and traditionalist attitude, which during Gandhi’s lifetime had given Indian politics a religious cast—misleadingly so, for, although Gandhi may have appeared to be a religious conservative, he was actually a social nonconformist trying to secularize Hinduism.<br/><br/>
+       </p>
+      </div>
+       
+  ,document.getElementById("bio"));
